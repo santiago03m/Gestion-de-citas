337b6b443f87d389e5cf29131b88650b9c43e895
SANTIAGO RUEDA DAVID <santiagorueda1116442@correo.itm.edu.co>
2025-10-12 19:01:01 -0500

    front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <a href="https://vite.dev" target="_blank" rel="noopener">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noopener">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
